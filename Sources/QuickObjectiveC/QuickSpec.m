4699d5af75c14a68a060f84f05ece0a38c292338
Álvaro Blázquez <alvarob25@gmail.com>
2022-04-04 11:59:13 +0200

    Fix xcode 13.3

--- Sources/QuickObjectiveC/QuickSpec.m
+++ Sources/QuickObjectiveC/QuickSpec.m
@@ -24,6 +24,9 @@ static QuickSpec *currentSpec = nil;
  @return An array of invocations that execute the newly defined example methods.
  */
 + (NSArray *)testInvocations {
+    // Xcode 13.3 hack, see this issue for more info: https://github.com/Quick/Quick/issues/1123
+    // In case of fix in later versions next line can be removed
+    [[QuickTestExamplesLoader sharedInstance] buildExamplesIfNeeded];
     NSArray *examples = [[World sharedWorld] examplesForSpecClass:[self class]];
     NSMutableArray *invocations = [NSMutableArray arrayWithCapacity:[examples count]];
     
@@ -163,5 +166,6 @@ static QuickSpec *currentSpec = nil;
 
 __attribute__((constructor))
 static void registerQuickTestObservation(void) {
-    [[XCTestObservationCenter sharedTestObservationCenter] addTestObserver:[QuickTestObservation sharedInstance]];
+    QuickTestObservation *observation = [QuickTestObservation new];
+    [[XCTestObservationCenter sharedTestObservationCenter] addTestObserver:observation];
 }
